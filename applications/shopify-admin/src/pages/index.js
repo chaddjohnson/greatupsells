fe58a8a2c7dce85026cefee1ca3921d79e50125b
Chad Johnson <chad.d.johnson@gmail.com>
2020-06-30 12:33:17 -0400

    Cleanup

--- applications/shopify-admin/src/pages/index.js
+++ applications/shopify-admin/src/pages/index.js
@@ -1,5 +1,4 @@
 import { memo } from 'react';
-import { useRouter } from 'next/router';
 import {
   Page,
   Layout,
@@ -29,87 +28,83 @@ const StatLabel = styled.div`
 
 const PageTitleBar = memo(() => <TitleBar title="Dashboard" />);
 
-const DashboardPage = () => {
-  const router = useRouter();
-
-  return (
-    <Page>
-      <PageTitleBar />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <StatsFlex>
-              <Stat>
-                <DisplayText size="extraLarge">96</DisplayText>
-                <StatLabel>
-                  <TextStyle variation="strong">Accepted offers</TextStyle>
-                </StatLabel>
-              </Stat>
-              <Stat>
-                <DisplayText size="extraLarge">$1,214.16</DisplayText>
-                <StatLabel>
-                  <TextStyle variation="strong">Revenue increase</TextStyle>
-                </StatLabel>
-              </Stat>
-              <Stat>
-                <DisplayText size="extraLarge">3.2%</DisplayText>
-                <StatLabel>
-                  <TextStyle variation="strong">Conversion rate</TextStyle>
-                </StatLabel>
-              </Stat>
-            </StatsFlex>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <Card
-            title="Accepted offers"
-            sectioned
-            actions={[{ content: 'View all analytics', url: '/analytics' }]}
-          >
-            <AcceptedOffersChart />
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <CalloutCard
-            title="Add upsell and cross-sell offers to your store"
-            primaryAction={{
-              content: 'Create offer',
-              url: '/offers/new'
+const DashboardPage = () => (
+  <Page>
+    <PageTitleBar />
+    <Layout>
+      <Layout.Section>
+        <Card sectioned>
+          <StatsFlex>
+            <Stat>
+              <DisplayText size="extraLarge">96</DisplayText>
+              <StatLabel>
+                <TextStyle variation="strong">Accepted offers</TextStyle>
+              </StatLabel>
+            </Stat>
+            <Stat>
+              <DisplayText size="extraLarge">$1,214.16</DisplayText>
+              <StatLabel>
+                <TextStyle variation="strong">Revenue increase</TextStyle>
+              </StatLabel>
+            </Stat>
+            <Stat>
+              <DisplayText size="extraLarge">3.2%</DisplayText>
+              <StatLabel>
+                <TextStyle variation="strong">Conversion rate</TextStyle>
+              </StatLabel>
+            </Stat>
+          </StatsFlex>
+        </Card>
+      </Layout.Section>
+      <Layout.Section>
+        <Card
+          title="Accepted offers"
+          sectioned
+          actions={[{ content: 'View all analytics', url: '/analytics' }]}
+        >
+          <AcceptedOffersChart />
+        </Card>
+      </Layout.Section>
+      <Layout.Section>
+        <CalloutCard
+          title="Add upsell and cross-sell offers to your store"
+          primaryAction={{
+            content: 'Create offer',
+            url: '/offers/new'
+          }}
+          secondaryAction={{
+            content: 'Manage your offers',
+            url: '/offers'
+          }}
+        >
+          <p>
+            Upselling and cross-selling are two of the most effective ways to
+            increase sales in your store.
+          </p>
+        </CalloutCard>
+        <MediaCard
+          title="Getting Started"
+          primaryAction={{
+            content: 'Visit the tutorials',
+            url: 'https://tutorials.domain.com'
+          }}
+          // description="Discover how Shopify can power up your entrepreneurial journey."
+          description="Learn how to use Great Upsells to boost your sales and revenue."
+        >
+          <img
+            alt=""
+            width="100%"
+            height="100%"
+            style={{
+              objectFit: 'cover',
+              objectPosition: 'center'
             }}
-            secondaryAction={{
-              content: 'Manage your offers',
-              url: '/offers'
-            }}
-          >
-            <p>
-              Upselling and cross-selling are two of the most effective ways to
-              increase sales in your store.
-            </p>
-          </CalloutCard>
-          <MediaCard
-            title="Getting Started"
-            primaryAction={{
-              content: 'Visit the tutorials',
-              url: 'https://tutorials.domain.com'
-            }}
-            // description="Discover how Shopify can power up your entrepreneurial journey."
-            description="Learn how to use Great Upsells to boost your sales and revenue."
-          >
-            <img
-              alt=""
-              width="100%"
-              height="100%"
-              style={{
-                objectFit: 'cover',
-                objectPosition: 'center'
-              }}
-              src="/images/tutorials.jpg"
-            />
-          </MediaCard>
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
-};
+            src="/images/tutorials.jpg"
+          />
+        </MediaCard>
+      </Layout.Section>
+    </Layout>
+  </Page>
+);
 
 export default DashboardPage;
