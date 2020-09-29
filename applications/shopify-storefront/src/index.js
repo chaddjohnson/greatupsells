cc34e93f67dc51bc070edfcc153d879d7b76d4e3
Chad Johnson <chad.d.johnson@gmail.com>
2020-09-28 20:52:11 -0400

    Reorganized and simplified Shopify Storefront application.

--- applications/shopify-storefront/src/index.js
+++ applications/shopify-storefront/src/index.js
@@ -1,2 +1,14 @@
-// import './modules/banner';
-import './modules/offerPopup';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+
+// TODO: Inject jQuery if unavailable and it is needed.
+
+const root = document.createElement('div');
+const rootId = 'upselling-popup-root';
+
+root.setAttribute('id', rootId);
+document.body.appendChild(root);
+
+ReactDOM.render(<App />, document.getElementById(rootId));
