e83322c57e05160d21a14524136a6aee63095184
Chad Johnson <chad.d.johnson@gmail.com>
2020-10-01 14:52:37 -0400

    Updated model getter module to include new models and getAll() method.

--- packages/models/src/models/index.js
+++ packages/models/src/models/index.js
@@ -4,13 +4,30 @@ const { loadModel } = require('@chaddjohnson/mongodb-client-lambda').loader;
 const { MONGODB_URI } = process.env;
 
 const pathsMap = {
+  Collection: path.join(__dirname, './Collection'),
   Offer: path.join(__dirname, './Offer'),
   OfferHit: path.join(__dirname, './OfferHit'),
-  // OfferProduct: path.join(__dirname, './OfferProduct'),
-  PopupTheme: path.join(__dirname, './PopupTheme'),
+  // PopupTheme: path.join(__dirname, './PopupTheme'),
   Product: path.join(__dirname, './Product'),
   Shop: path.join(__dirname, './Shop'),
-  Stat: path.join(__dirname, './Stat')
+  // Stat: path.join(__dirname, './Stat'),
+  User: path.join(__dirname, './User')
 };
 
-module.exports.get = async (name) => loadModel(name, pathsMap, MONGODB_URI);
+const get = async (name) => loadModel(name, pathsMap, MONGODB_URI);
+
+const getAll = async () => ({
+  Collection: await get('Collection'),
+  Offer: await get('Offer'),
+  OfferHit: await get('OfferHit'),
+  // PopupTheme: await get('PopupTheme'),
+  Product: await get('Product'),
+  Shop: await get('Shop'),
+  // Stat: await get('Stat'),
+  User: await get('User')
+});
+
+module.exports = {
+  get,
+  getAll
+};
