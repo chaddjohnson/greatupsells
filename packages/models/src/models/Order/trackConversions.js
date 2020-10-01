11e563eb364c8b0c89bf214ee006e793053a2b14
Chad Johnson <chad.d.johnson@gmail.com>
2020-10-01 14:57:34 -0400

    Added a way to track conversions for an order.

---
+++ packages/models/src/models/Order/trackConversions.js
@@ -0,0 +1,31 @@
+const Promise = require('bluebird');
+const { compact } = require('lodash');
+const models = require('..');
+
+const trackConversions = async (order) => {
+  const OfferHit = await models.get('OfferHit');
+
+  // Get line items for the order.
+  const lineItems = order.shopifyOrderData.line_items || [];
+
+  // Find all offer hits associated with line items.
+  const offerHits = compact(
+    await Promise.all(
+      lineItems.map(async (lineItem) =>
+        OfferHit.findByAcceptedShopifyProductId(lineItem.id)
+      )
+    )
+  );
+
+  // Track conversions for offer hits
+  await Promise.all(offerHits.map((offerHit) => offerHit.trackConversion()));
+
+  // Track revenue increase for order
+  order.revenueIncrease = offerHits.reduce((sum, offerHit) => {
+    return sum + (offerHit.revenueIncrease || 0);
+  }, 0);
+
+  await order.save();
+};
+
+module.exports = trackConversions;
