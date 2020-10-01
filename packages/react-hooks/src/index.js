8ac61fe64a557c1fa23cd753d68b9bfabf6d57a8
Chad Johnson <chad.d.johnson@gmail.com>
2020-10-01 14:05:32 -0400

    Updated index for hooks.

--- packages/react-hooks/src/index.js
+++ packages/react-hooks/src/index.js
@@ -1,5 +1,7 @@
 export { ShopProvider, useShop } from './shop';
 
+export { default as useCookies } from './cookies';
+export { default as useDateFormatter } from './dateFormatter';
 export { default as useNumberFormatter } from './numberFormatter';
 export { default as useOffer } from './offer';
 export { default as useOfferAcceptances } from './offerAcceptances';
@@ -9,4 +11,5 @@ export { default as useOfferConversions } from './offerConversions';
 export { default as useOfferRevenueIncreases } from './offerRevenueIncreases';
 export { default as useOfferViews } from './offerViews';
 export { default as useOffers } from './offers';
+export { default as useRandomOffer } from './randomOffer';
 export { default as useToast } from './toast';
