209d13048d697c1d3effae57a80d7a98a773db80
Chad Johnson <chad.d.johnson@gmail.com>
2020-10-01 15:13:12 -0400

    Moved GraphQL client into its own package, and created hooks and added context provider to provide an interface similar to Apollo Client.

---
+++ packages/graphql-client/src/client.js
@@ -0,0 +1,40 @@
+import { GraphQLClient as GraphQLRequestClient } from 'graphql-request';
+
+class GraphQLClient {
+  constructor({ uri, interceptors, ...clientOptions }) {
+    this.interceptors = {
+      request: (interceptors && interceptors.request) || (() => {})
+    };
+    this.config = { headers: {} };
+    this.client = new GraphQLRequestClient(uri, clientOptions);
+  }
+
+  async query(queryString, variables = {}) {
+    this.interceptors.request(this.config);
+
+    // This throws an error if the response contains an error.
+    const response = await this.client.request(queryString, variables || {});
+
+    try {
+      // Determine the keys returned.
+      const responseKeys = response && Object.keys(response);
+
+      // Return the first object if there is only one.
+      if (responseKeys?.length === 1) {
+        return response[responseKeys[0]];
+      }
+
+      // Return all objects by default.
+      return response;
+    } catch (error) {
+      // Return the original response if there was an issue working with the response.
+      return response;
+    }
+  }
+
+  async mutate(queryString, variables = {}) {
+    return this.query(queryString, variables);
+  }
+}
+
+export default GraphQLClient;
