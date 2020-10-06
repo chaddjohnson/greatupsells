33a9d7d6fad2bfd52e0ab50d86874821774c1f82
Chad Johnson <chad.d.johnson@gmail.com>
2020-10-05 22:14:30 -0400

    Added queue infrastructure for product and collection import, and enqueuing messages on collection and product import.

--- packages/models/src/Shop/importCollections.js
+++ packages/models/src/Shop/importCollections.js
@@ -1,14 +1,26 @@
+const AWS = require('aws-sdk');
+
+const { SHOP_COLLECTION_IMPORT_QUEUE_URL } = process.env;
+
 const importCollections = async (shop) => {
+  const sqs = new AWS.SQS();
   const shopCollectionCount = await shop.getCollectionCount();
+  const { shopifyShopId } = shop;
 
   // Abort import if collections have already been imported.
   if (shopCollectionCount > 1) {
     return;
   }
 
-  // TODO: Enqueue a background worker via SQS.
-  // TODO: Ignore or update existing.
-  return undefined; // TODO
+  // Enqueue a background worker.
+  await sqs
+    .sendMessage({
+      QueueUrl: SHOP_COLLECTION_IMPORT_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        shopifyShopId
+      })
+    })
+    .promise();
 };
 
 module.exports = importCollections;
--- packages/models/src/Shop/importProducts.js
+++ packages/models/src/Shop/importProducts.js
@@ -1,14 +1,26 @@
+const AWS = require('aws-sdk');
+
+const { SHOP_PRODUCT_IMPORT_QUEUE_URL } = process.env;
+
 const importProducts = async (shop) => {
+  const sqs = new AWS.SQS();
   const shopProductCount = await shop.getProductCount();
+  const { shopifyShopId } = shop;
 
   // Abort import if products have already been imported.
   if (shopProductCount > 1) {
     return;
   }
 
-  // TODO: Enqueue a background worker via SQS.
-  // TODO: Ignore or update existing.
-  return undefined; // TODO
+  // Enqueue a background worker.
+  await sqs
+    .sendMessage({
+      QueueUrl: SHOP_PRODUCT_IMPORT_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        shopifyShopId
+      })
+    })
+    .promise();
 };
 
 module.exports = importProducts;
