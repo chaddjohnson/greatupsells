37b1ce0a8e9a2503d849c814dc686dd56be0aca1
Chad Johnson <chad.d.johnson@gmail.com>
2020-10-07 21:09:29 -0400

    Updating local Shopify order data copy before handling order cancelation.

--- applications/webhooks/src/routes/shopify/orderCancelation.js
+++ applications/webhooks/src/routes/shopify/orderCancelation.js
@@ -17,12 +17,12 @@ const handler = async (request, response) => {
 
   try {
     if (order && !order.canceledAt) {
-      // Handle order cancelation.
-      await order.cancel();
-
       // Update local Shopify data for the order.
       // Use one round trip to prevent write conflicts.
       await Order.findByIdAndUpdate(data.id, { shopifyOrderData: data });
+
+      // Handle order cancelation.
+      await order.cancel();
     }
   } catch (error) {
     logger.error(
