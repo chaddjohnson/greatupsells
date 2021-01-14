c7c1031ce505b2248bc70d9c63b0e7d4662e7d60
Chad Johnson <chad.d.johnson@gmail.com>
2021-01-14 11:34:49 -0500

    Added missing route.

---
+++ services/shops/src/routes/findOneOrder.js
@@ -0,0 +1,34 @@
+const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+const logger = require('@neatowebsolutions/upselling-logger');
+const models = require('../../models');
+
+const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    const { orderId } = event.pathParameters;
+    const Order = await models.get('Order');
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return {
+        statusCode: StatusCodes.NOT_FOUND,
+        body: ReasonPhrases.NOT_FOUND
+      };
+    }
+
+    return {
+      statusCode: StatusCodes.OK,
+      body: JSON.stringify(order)
+    };
+  } catch (error) {
+    await logger.error(`Error retrieving order`, error, event);
+
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: ReasonPhrases.INTERNAL_SERVER_ERROR
+    };
+  }
+};
+
+module.exports.handler = handler;
