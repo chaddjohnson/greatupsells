8e2f5ba2eb53d6c702a5a6f9d6a99b72ac9a14c1
Chad Johnson <chad.d.johnson@gmail.com>
2021-01-14 10:58:05 -0500

    Removed Graphql stuff.

--- applications/shop-api-gateway/src (OLD)/schema/index.js
+++
@@ -1,5 +0,0 @@
-const { makeExecutableSchema } = require('@graphql-tools/schema');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-
-module.exports = makeExecutableSchema({ typeDefs, resolvers });
