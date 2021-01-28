b309d9334ae658b2faf209ccfb04ca0bc8aa6968
Chad Johnson <chad.d.johnson@gmail.com>
2021-01-27 21:03:03 -0500

    Implemented HMAC validation for webhooks.

--- services/webhooks/src/workers/appUninstall.js
+++ services/webhooks/src/workers/appUninstall.js
@@ -1,7 +1,11 @@
+const {
+  checkWebhookHmacValidity,
+  createRawBody
+} = require('shopify-hmac-validation');
 const HttpClient = require('@neatowebsolutions/upselling-http-client');
 const logger = require('@neatowebsolutions/upselling-logger');
 
-const { SHOPS_API_URL } = process.env;
+const { SHOPS_API_URL, SHOPIFY_ADMIN_API_SECRET_KEY } = process.env;
 
 const httpClient = new HttpClient({
   baseUrl: SHOPS_API_URL
@@ -9,8 +13,9 @@ const httpClient = new HttpClient({
 
 const processRecord = async (record) => {
   try {
-    const { detail } = JSON.parse(record.body);
-    const { metadata, errors } = detail;
+    const body = JSON.parse(record.body);
+    const { detail } = body;
+    const { payload, metadata, errors } = detail;
 
     if (errors) {
       return await logger.error(
@@ -18,6 +23,17 @@ const processRecord = async (record) => {
         errors,
         record
       );
+    }
+
+    const hmac = metadata['X-Shopify-Hmac-SHA256'];
+    const hmacValid = checkWebhookHmacValidity(
+      SHOPIFY_ADMIN_API_SECRET_KEY,
+      createRawBody(payload),
+      hmac
+    );
+
+    if (!hmacValid) {
+      return await logger.error('Invalid HMAC for webhook', record);
     }
 
     const domain = metadata['X-Shopify-Shop-Domain'];
