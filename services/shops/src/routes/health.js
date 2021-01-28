c01bf322da9666ef5beda01451e4d27ded9eae35
Chad Johnson <chad.d.johnson@gmail.com>
2021-01-27 21:53:24 -0500

    Fleshed out Shops Service health check.

--- services/shops/src/routes/health.js
+++ services/shops/src/routes/health.js
@@ -1,8 +1,26 @@
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+const mongodbClient = require('../models/mongodbClient');
 
-const handler = async () => ({
-  statusCode: StatusCodes.OK,
-  body: ReasonPhrases.OK
-});
+const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    await mongodbClient.connect();
+
+    if (!mongodbClient.connected) {
+      throw new Error(`Cannot connect to MongoDB`);
+    }
+
+    return {
+      statusCode: StatusCodes.OK,
+      body: ReasonPhrases.OK
+    };
+  } catch (error) {
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+    };
+  }
+};
 
 module.exports.handler = handler;
