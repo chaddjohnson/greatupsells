eb2a5daa3384ba4071acfa51f07566db869fbb70
Chad Johnson <chad.d.johnson@gmail.com>
2021-01-28 13:17:38 -0500

    Added CORS headers for region endpoint.

--- applications/shopify-admin/server/region.js
+++ applications/shopify-admin/server/region.js
@@ -1,10 +1,14 @@
+const middy = require('@middy/core');
+const cors = require('@middy/http-cors');
 const { StatusCodes } = require('http-status-codes');
 
 const { AWS_REGION } = process.env;
 
-module.exports.handler = async () => {
-  return {
-    statusCode: StatusCodes.OK,
-    body: AWS_REGION
-  };
-};
+const handler = middy(async () => ({
+  statusCode: StatusCodes.OK,
+  body: AWS_REGION
+}));
+
+handler.use(cors());
+
+module.exports.handler = handler;
