c8863b652766702b7ac66e851ee3923d505122ec
Chad Johnson <chad.d.johnson@gmail.com>
2021-01-30 18:32:06 -0500

    Using variable for region in Shops Service infrastructure.

--- services/shops/infrastructure/main.tf
+++ services/shops/infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "neatowebsolutions-upselling-infrastructure"
     key    = "shops-service.tfstate"
-    region = "us-east-1"
+    region = var.region
   }
 }
 
