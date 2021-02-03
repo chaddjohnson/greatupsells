73295e54d602e8b5f3da92b30259efc62dde842e
Chad Johnson <chad.d.johnson@gmail.com>
2021-02-02 21:24:54 -0500

    Hard-coding region for Logs Service.

--- services/logs/infrastructure/main.tf
+++ services/logs/infrastructure/main.tf
@@ -7,12 +7,12 @@ terraform {
 }
 
 provider "aws" {
-  region                      = var.region
+  version                     = "~> 3.18"
+  region                      = "us-east-1"
   s3_force_path_style         = terraform.workspace == "dev"
   skip_credentials_validation = terraform.workspace == "dev"
   skip_metadata_api_check     = terraform.workspace == "dev"
   skip_requesting_account_id  = terraform.workspace == "dev"
-  version                     = "~> 3.18"
 
   endpoints {
     sqs = terraform.workspace == "dev" ? "http://localhost:4566" : null
