bd8fe1fe1334988e0ae44acc87e11dc264cabe64
Chad Johnson <chad.d.johnson@gmail.com>
2021-03-18 20:35:25 -0400

    Cleanup

--- packages/react-components/src/OfferPopup.js
+++ packages/react-components/src/OfferPopup.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
 import styled from 'styled-components';
