20acc9234f4daae0c1c90688ecb1b7a1da092359
Chad Johnson <chad.d.johnson@gmail.com>
2021-04-13 13:13:44 -0400

    Fixed bad reference.

--- packages/react-hooks/src/httpClient.js
+++ packages/react-hooks/src/httpClient.js
@@ -11,7 +11,7 @@ const HttpClientProvider = ({ httpClient, children }) => (
 );
 
 HttpClientProvider.propTypes = {
-  client: PropTypes.object.isRequired,
+  httpClient: PropTypes.object.isRequired,
   children: PropTypes.node.isRequired
 };
 
