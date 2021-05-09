1c8e757c7e1fb48747cd9f1aed55bc5316d84216
Chad Johnson <chad.d.johnson@gmail.com>
2021-05-08 21:04:17 -0400

    Exporting LostBrowserFocusOffer.

--- applications/shopify-storefront/src/components/index.js
+++ applications/shopify-storefront/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as AnyPageLoadOffer } from './AnyPageLoadOffer';
 export { default as CartOffer } from './CartOffer';
 export { default as ExitIntentOffer } from './ExitIntentOffer';
+export { default as LostBrowserFocusOffer } from './LostBrowserFocusOffer';
 export { default as ProductOffer } from './ProductOffer';
 export { default as SpecificPageLoadOffer } from './SpecificPageLoadOffer';
