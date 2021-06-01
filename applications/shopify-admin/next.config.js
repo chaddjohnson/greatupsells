1245d171073e8e328cb55841b2f14935e26e1bb9
Chad Johnson <chad.d.johnson@gmail.com>
2021-06-01 11:20:07 -0400

    Removed unnecessary Next.js config.

--- applications/shopify-admin/next.config.js
+++ applications/shopify-admin/next.config.js
@@ -38,25 +38,6 @@ module.exports = {
       type: 'javascript/auto'
     });
 
-    // Necessary to use symlinked packages (Lerna creates symlinks in node_modules).
-    config.resolve.symlinks = false;
-
-    return config;
-  },
-
-  webpackDevMiddleware: (config) => {
-    // Don't ignore all node modules.
-    config.watchOptions.ignored = config.watchOptions.ignored.filter(
-      (ignore) => !ignore.toString().includes('node_modules')
-    );
-
-    // Ignore all node modules except those here.
-    config.watchOptions.ignored = [
-      ...config.watchOptions.ignored,
-      'node_modules/!(@neatowebsolutions)',
-      '@neatowebsolutions/*/node_modules'
-    ];
-
     return config;
   },
 
