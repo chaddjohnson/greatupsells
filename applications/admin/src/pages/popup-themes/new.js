025899f09a16c6c2018fd6301cb3121ec161c79d
Chad Johnson <chad.d.johnson@gmail.com>
2021-07-05 18:44:46 -0400

    Fixed and improved New page for popup themes.

--- applications/admin/src/pages/popup-themes/new.js
+++ applications/admin/src/pages/popup-themes/new.js
@@ -1,50 +1,52 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { Breadcrumbs, Hidden } from '@material-ui/core';
 import { Brush as PopupThemesIcon } from '@material-ui/icons';
 import { Layout, Link, PopupThemeEditor } from '../../components';
 import { usePopupTheme } from '../../hooks';
 
-const initialTemplate = `
-<style>
-  .popup-container {
-    font-family: {{ bodyFont }};
-    font-size: {{ bodyFontSize }}px;
-    color: black;
-    background-color: {{ popupBackgroundColor }};
-    width: 650px;
-    height: auto;
-    max-width: 100%;
-    max-height: 100%;
-    margin: auto;
-    box-sizing: border-box;
-    position: relative;
-
-    /* Reference: https://gist.github.com/hsleonis/55712b0eafc9b25f1944 */
-    font-size: 100%;
-    -webkit-text-size-adjust: 100%;
-    font-variant-ligatures: none;
-    -webkit-font-variant-ligatures: none;
-    text-rendering: optimizeLegibility;
-    -moz-osx-font-smoothing: grayscale;
-    font-smoothing: antialiased;
-    -webkit-font-smoothing: antialiased;
-    text-shadow: rgba(0, 0, 0, .01) 0 0 1px;
-
-    /* Reference: https://gist.github.com/chemicaloliver/1234670 */
-    border: 1px solid rgba(0, 0, 0, 0.3);
-    -webkit-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
-    -moz-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
-    box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
-    -webkit-background-clip: padding-box;
-    -moz-background-clip: padding-box;
-    background-clip: padding-box;
-  }
-</style>
+const initialHtmlTemplate = `
 <form novalidate onsubmit="{{ submitHandler }}">
-  <div class="popup-container">
+  <div class="container">
     Hello World
   </div>
 </form>
+`.trim();
+
+const initialCssTemplate = `
+.container {
+  font-family: {{ bodyFont }};
+  font-size: {{ bodyFontSize }}px;
+  color: black;
+  background-color: {{ popupBackgroundColor }};
+  width: 650px;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+  margin: auto;
+  box-sizing: border-box;
+  position: relative;
+
+  /* Reference: https://gist.github.com/hsleonis/55712b0eafc9b25f1944 */
+  font-size: 100%;
+  -webkit-text-size-adjust: 100%;
+  font-variant-ligatures: none;
+  -webkit-font-variant-ligatures: none;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;
+  font-smoothing: antialiased;
+  -webkit-font-smoothing: antialiased;
+  text-shadow: rgba(0, 0, 0, .01) 0 0 1px;
+
+  /* Reference: https://gist.github.com/chemicaloliver/1234670 */
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+  -moz-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+  -webkit-background-clip: padding-box;
+  -moz-background-clip: padding-box;
+  background-clip: padding-box;
+}
 `.trim();
 
 const initialVariables = [
@@ -91,20 +93,27 @@ const initialVariables = [
 const initialValues = {
   name: '',
   displayOrder: 1,
-  type: 'UPSELL',
+  strategy: 'UPSELL',
   category: '',
   thumbnailImageUrl: '',
   description: '',
-  template: initialTemplate,
+  template: {
+    html: initialHtmlTemplate,
+    css: initialCssTemplate,
+    javascript: ''
+  },
   variables: initialVariables,
   formFields: []
 };
 
 const NewPopupThemePage = () => {
+  const router = useRouter();
   const { savePopupTheme } = usePopupTheme();
 
   const handleSubmit = async (values) => {
-    await savePopupTheme(values);
+    const popupTheme = await savePopupTheme(values);
+
+    router.push(`/popup-themes/${popupTheme._id}`);
   };
 
   return (
