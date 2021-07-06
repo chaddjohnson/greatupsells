a272a2f1d8d9a9c855574bca4d6cd14143c2a5d9
Chad Johnson <chad.d.johnson@gmail.com>
2021-07-05 23:59:27 -0400

    Implemented the ability to export popup theme HTML.

--- applications/admin/src/components/PopupThemeList/PopupThemeMenu.js
+++ applications/admin/src/components/PopupThemeList/PopupThemeMenu.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { MoreVert as MoreVertIcon } from '@material-ui/icons';
+import clsx from 'clsx';
+import Link from '../Link';
+import Preview from '../PopupThemeEditor/PopupTemplateEditor/Preview';
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    color: theme.palette.text.primary
+  },
+  preview: {
+    display: 'none'
+  }
+}));
 
 const PopupThemeMenu = ({ popupTheme, onClonePopupTheme }) => {
+  const classes = useStyles();
+
   const [menuAnchorElement, setMenuAnchorElement] = useState(null);
+  const [popupThemeHtml, setPopupThemeHtml] = useState();
+
   const menuOpen = Boolean(menuAnchorElement);
 
+  const previewContainerElement = useRef(null);
+
   const handleMenuOpen = (event) => {
+    const iframe = previewContainerElement.current.querySelector('.preview');
+    const html = iframe?.contentWindow.document.documentElement.outerHTML;
+
+    setPopupThemeHtml(encodeURIComponent(html));
     setMenuAnchorElement(event.currentTarget);
   };
 
@@ -16,8 +39,18 @@ const PopupThemeMenu = ({ popupTheme, onClonePopupTheme }) => {
   };
 
   const handleCloneTheme = async () => {
+    setMenuAnchorElement(null);
     await onClonePopupTheme(popupTheme);
   };
+
+  const exportFileName = useMemo(
+    () =>
+      `${popupTheme.name
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9\- ]/g, '')
+        .replace(/\s+/g, '-')}.html`,
+    [popupTheme]
+  );
 
   return (
     <>
@@ -37,7 +70,22 @@ const PopupThemeMenu = ({ popupTheme, onClonePopupTheme }) => {
         }}
       >
         <MenuItem onClick={handleCloneTheme}>Clone</MenuItem>
+        <MenuItem
+          className={classes.link}
+          component={Link}
+          href={`data:text/html;charset=utf-8,${popupThemeHtml}`}
+          download={exportFileName}
+          onClick={handleMenuClose}
+        >
+          Export HTML
+        </MenuItem>
       </Menu>
+      <div ref={previewContainerElement}>
+        <Preview
+          className={clsx(classes.preview, 'preview')}
+          popupTheme={popupTheme}
+        />
+      </div>
     </>
   );
 };
