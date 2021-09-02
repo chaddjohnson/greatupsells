4b1af04c4b47018f4351b14706291796ebd3d17c
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-02 17:45:41 -0400

    Fleshed out infrastructure some more.

--- services/shops/infrastructure/main.tf
+++ services/shops/infrastructure/main.tf
@@ -2,11 +2,10 @@ terraform {
   backend "s3" {
     bucket = "neatowebsolutions-upselling-infrastructure"
     key    = "shops-service.tfstate"
-    region = var.region
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  version = "~> 3.18"
-  region  = var.region
+  region = "us-east-1"
 }
--- services/shops/infrastructure/queues.tf
+++ services/shops/infrastructure/queues.tf
@@ -6,6 +6,40 @@ resource "aws_sqs_queue" "shop_product_import" {
   name = "shop-product-import-queue-${terraform.workspace}"
 }
 
+resource "aws_sqs_queue_policy" "shop_collection_import_policy" {
+  queue_url = "${aws_sqs_queue.shop_collection_import.id}"
+  policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.shop_collection_import.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_sqs_queue_policy" "shop_product_import_policy" {
+  queue_url = "${aws_sqs_queue.shop_product_import.id}"
+  policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.shop_product_import.arn}"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ssm_parameter" "shop_collection_import_queue_arn" {
   name  = "/upselling/${terraform.workspace}/queues/shop-collection-import/arn"
   type  = "String"
