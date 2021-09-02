f111e2b4428536fb1a1d84b0c906b020d2f88d04
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-02 18:14:07 -0400

    Using infrastructure script.

--- services/shops/scripts/infrastructure.sh
+++ services/shops/scripts/infrastructure.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# [ -z "$STAGE" ] && echo "STAGE not set" && exit 1;
-# [ -z "$AWS_REGION" ] && echo "AWS_REGION not set" && exit 1;
+[ -z "$STAGE" ] && echo "STAGE not set" && exit 1;
 
-# terraform init infrastructure
-# terraform validate infrastructure
-# terraform workspace select $STAGE infrastructure || { terraform workspace new $STAGE infrastructure && terraform init infrastructure; }
-# terraform apply -var 'region=$AWS_REGION' -auto-approve infrastructure
+terraform init infrastructure
+terraform validate infrastructure
+terraform workspace select $STAGE infrastructure || { terraform workspace new $STAGE infrastructure && terraform init infrastructure; }
+terraform apply -var-file=config/$STAGE.tfvars -auto-approve infrastructure
