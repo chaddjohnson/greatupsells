8a6e6cb97a425e4722325d92707f5abae6b18f79
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-03 13:16:18 -0400

    Fixed role config.

--- services/shops/infrastructure/roles.tf
+++ services/shops/infrastructure/roles.tf
@@ -1,22 +1,42 @@
 resource "aws_iam_role" "services_consumer_role" {
-  name               = "services_consumer_role"
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:CreateLogGroup",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "*"
-        }
+  name = "services_consumer_role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : ["lambda.amazonaws.com"]
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
     ]
+  })
 }
-EOF
+
+resource "aws_iam_policy" "services_consumer_role_policy" {
+  name = "services_consumer_role_policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "services_consumer_role_policy" {
+  role       = aws_iam_role.services_consumer_role.name
+  policy_arn = aws_iam_policy.services_consumer_role_policy.arn
 }
 
 resource "aws_ssm_parameter" "services_consumer_role_arn" {
