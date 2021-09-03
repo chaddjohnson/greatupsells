61e356870dd16e5fd5706d2688be440c9c45ec81
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-03 13:16:40 -0400

    Moved infrastructure run script.

--- services/shops/scripts/infrastructure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ -z "$STAGE" ] && echo "STAGE not set" && exit 1;
-
-terraform -chdir=./infrastructure init
-terraform validate infrastructure
-terraform workspace select $STAGE infrastructure || { terraform workspace new $STAGE infrastructure && terraform init infrastructure; }
-terraform apply -var-file=config/$STAGE.tfvars -auto-approve infrastructure
