35d4814c5c0c0ce5de33bb9c1764fe6342dc8f4c
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-03 11:08:19 -0400

    Updated infrastructure script.

--- services/shops/scripts/infrastructure.sh
+++ services/shops/scripts/infrastructure.sh
@@ -2,7 +2,7 @@
 
 [ -z "$STAGE" ] && echo "STAGE not set" && exit 1;
 
-terraform init infrastructure
+terraform -chdir=./infrastructure init
 terraform validate infrastructure
 terraform workspace select $STAGE infrastructure || { terraform workspace new $STAGE infrastructure && terraform init infrastructure; }
 terraform apply -var-file=config/$STAGE.tfvars -auto-approve infrastructure
