e6bb93b727eb3b2f6d9a6455abbbe1c1e6a362ff
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-08 13:44:33 -0400

    Updated security group settings for services server.

--- infrastructure/servicesServer.tf
+++ infrastructure/servicesServer.tf
@@ -29,10 +29,10 @@ resource "aws_security_group" "services_server" {
   }
 
   ingress {
-    from_port       = 27017
-    to_port         = 27017
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda_functions_security_group.id]
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
