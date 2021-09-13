71bb75607680218004660842a52e3c264f402514
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-12 23:26:47 -0400

    Added missing infrastructure config.

---
+++ services/storefront-api-gateway/infrastructure/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "neatowebsolutions-upselling-infrastructure"
+    key    = "storefront-api-gateway.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
