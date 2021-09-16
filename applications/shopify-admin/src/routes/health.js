51369b304a64535a31ff5ff99c05e8d535456773
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-15 23:01:26 -0400

    Updated /health routes to not depend on other services.

--- applications/shopify-admin/src/routes/health.js
+++ applications/shopify-admin/src/routes/health.js
@@ -1,39 +1,11 @@
 const middy = require('@middy/core');
 const cors = require('@middy/http-cors');
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
-const HttpClient = require('@neatowebsolutions/upselling-http-client').default;
 
-const { SHOPS_API_URL, SHOPIFY_ADMIN_API_URL } = process.env;
-
-const shopsServiceHttpClient = new HttpClient({
-  baseUrl: SHOPS_API_URL
-});
-const shopifyAdminApiHttpClient = new HttpClient({
-  baseUrl: SHOPIFY_ADMIN_API_URL
-});
-
-const handler = middy(async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  try {
-    await Promise.all([
-      await shopsServiceHttpClient.get('/health'),
-      await shopifyAdminApiHttpClient.get('/health')
-    ]);
-
-    return {
-      statusCode: StatusCodes.OK,
-      body: ReasonPhrases.OK
-    };
-  } catch (error) {
-    console.error(error.stack); // eslint-disable-line no-console
-
-    return {
-      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-      body: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
-    };
-  }
-});
+const handler = middy(async () => ({
+  statusCode: StatusCodes.OK,
+  body: ReasonPhrases.OK
+}));
 
 handler.use(cors());
 
--- services/webhooks/src/routes/health.js
+++ services/webhooks/src/routes/health.js
@@ -1,33 +1,11 @@
 const middy = require('@middy/core');
 const cors = require('@middy/http-cors');
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
-const HttpClient = require('@neatowebsolutions/upselling-http-client').default;
 
-const { SHOPS_API_URL } = process.env;
-
-const httpClient = new HttpClient({
-  baseUrl: SHOPS_API_URL
-});
-
-const handler = middy(async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  try {
-    await httpClient.get('/health');
-
-    return {
-      statusCode: StatusCodes.OK,
-      body: ReasonPhrases.OK
-    };
-  } catch (error) {
-    console.error(error.stack); // eslint-disable-line no-console
-
-    return {
-      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-      body: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
-    };
-  }
-});
+const handler = middy(async () => ({
+  statusCode: StatusCodes.OK,
+  body: ReasonPhrases.OK
+}));
 
 handler.use(cors());
 
