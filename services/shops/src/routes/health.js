29ccab17eae38798e67469ebee5ab86ae75f9cf7
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-17 14:24:45 -0400

    debugging

--- services/shops/src/routes/health.js
+++ services/shops/src/routes/health.js
@@ -3,20 +3,25 @@ const mongodbClient = require('../models/mongodbClient');
 
 const handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-
+  console.log('health 1');
   try {
     await mongodbClient.connect();
+    console.log('health 2');
 
     if (!mongodbClient.connected) {
       throw new Error(`Cannot connect to MongoDB`);
     }
+    console.log('health 3');
 
     return {
       statusCode: StatusCodes.OK,
       body: ReasonPhrases.OK
     };
   } catch (error) {
+    console.log('health 4');
+    console.log(error);
     console.error(error.stack); // eslint-disable-line no-console
+    console.log('health 5');
 
     return {
       statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
