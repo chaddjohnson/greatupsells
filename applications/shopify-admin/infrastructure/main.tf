51577ce08f478d6fde8f3d6d51e64be079aa1399
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-17 14:04:47 -0400

    Fixed Terraform backend file references.

--- applications/shopify-admin/infrastructure/main.tf
+++ applications/shopify-admin/infrastructure/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "neatowebsolutions-upselling-infrastructure"
-    key    = "storefront-api.tfstate"
+    key    = "shopify-admin.tfstate"
     region = "us-east-1"
   }
 }
