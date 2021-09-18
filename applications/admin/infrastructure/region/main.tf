41eba728be51c4726c52eb8472d3af2c664e7d7f
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-18 00:52:54 -0400

    Using aliases instead of CNAMEs.

--- applications/admin/infrastructure/region/main.tf
+++ applications/admin/infrastructure/region/main.tf
@@ -138,11 +138,15 @@ resource "aws_route53_health_check" "admin_app" {
 resource "aws_route53_record" "admin_app" {
   zone_id         = data.terraform_remote_state.upselling_infrastructure.outputs.hosted_zone_id
   name            = var.admin_app_domain
-  type            = "CNAME"
-  ttl             = "86400"
+  type            = "A"
   set_identifier  = data.aws_region.current.name
-  records         = [aws_cloudfront_distribution.admin_app.domain_name]
   health_check_id = aws_route53_health_check.admin_app.id
+
+  alias {
+    name                   = aws_cloudfront_distribution.admin_app.domain_name
+    zone_id                = aws_cloudfront_distribution.admin_app.hosted_zone_id
+    evaluate_target_health = false
+  }
 
   latency_routing_policy {
     region = data.aws_region.current.name
