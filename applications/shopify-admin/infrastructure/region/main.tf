36be0f026db240c48cecc273db5fc7b0339f98fe
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-17 21:58:24 -0400

    Using CloudFront to proxy to regional API gateways for applications.

--- applications/shopify-admin/infrastructure/region/main.tf
+++ applications/shopify-admin/infrastructure/region/main.tf
@@ -21,19 +21,109 @@ data "terraform_remote_state" "upselling_infrastructure" {
 }
 
 resource "aws_ssm_parameter" "shopify_admin_app_regional_domain" {
-  name     = "/upselling/${terraform.workspace}/shopify-admin-app/regional-domain"
-  type     = "String"
-  value    = "shopify-admin.${data.aws_region.current.name}.${data.terraform_remote_state.upselling_infrastructure.outputs.domain}"
+  name      = "/upselling/${terraform.workspace}/shopify-admin-app/regional-domain"
+  type      = "String"
+  value     = "shopify-admin.${data.aws_region.current.name}.${data.terraform_remote_state.upselling_infrastructure.outputs.domain}"
   overwrite = true
-  provider = aws.region
+  provider  = aws.region
 }
 
 resource "aws_ssm_parameter" "shopify_admin_app_url" {
-  name     = "/upselling/${terraform.workspace}/shopify-admin-app/url"
-  type     = "String"
-  value    = "https://${var.shopify_admin_app_domain}"
+  name      = "/upselling/${terraform.workspace}/shopify-admin-app/url"
+  type      = "String"
+  value     = "https://${var.shopify_admin_app_domain}"
   overwrite = true
-  provider = aws.region
+  provider  = aws.region
+}
+
+resource "aws_cloudfront_distribution" "shopify_admin_app" {
+  enabled     = true
+  aliases     = [var.shopify_admin_app_domain]
+  price_class = "PriceClass_All"
+
+  origin {
+    domain_name = aws_ssm_parameter.shopify_admin_app_regional_domain.value
+    origin_id   = "api"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "api"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = false
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  custom_error_response {
+    error_code            = 400
+    error_caching_min_ttl = 5
+  }
+
+  custom_error_response {
+    error_code            = 403
+    error_caching_min_ttl = 5
+  }
+
+  custom_error_response {
+    error_code            = 404
+    error_caching_min_ttl = 5
+  }
+
+  custom_error_response {
+    error_code            = 500
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 501
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 502
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 503
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 504
+    error_caching_min_ttl = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = data.terraform_remote_state.upselling_infrastructure.outputs.certificate_arn
+    minimum_protocol_version = "TLSv1.2_2018"
+    ssl_support_method       = "sni-only"
+  }
 }
 
 resource "aws_route53_health_check" "shopify_admin_app" {
@@ -51,7 +141,7 @@ resource "aws_route53_record" "shopify_admin_app" {
   type            = "CNAME"
   ttl             = "86400"
   set_identifier  = data.aws_region.current.name
-  records         = [aws_ssm_parameter.shopify_admin_app_regional_domain.value]
+  records         = [aws_cloudfront_distribution.shopify_admin_app.domain_name]
   health_check_id = aws_route53_health_check.shopify_admin_app.id
 
   latency_routing_policy {
