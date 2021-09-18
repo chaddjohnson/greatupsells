406b361eafaa251e797cfdb2ff8f108bc4658618
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-17 21:42:57 -0400

    Using CloudFront to proxy to regional API gateways.

--- services/storefront-api/infrastructure/region/main.tf
+++ services/storefront-api/infrastructure/region/main.tf
@@ -36,6 +36,96 @@ resource "aws_ssm_parameter" "storefront_api_url" {
   provider  = aws.region
 }
 
+resource "aws_cloudfront_distribution" "storefront_api" {
+  enabled     = true
+  aliases     = [var.storefront_api_domain]
+  price_class = "PriceClass_All"
+
+  origin {
+    domain_name = aws_ssm_parameter.storefront_api_regional_domain.value
+    origin_id   = "api"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "api"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = false
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  custom_error_response {
+    error_code            = 400
+    error_caching_min_ttl = 5
+  }
+
+  custom_error_response {
+    error_code            = 403
+    error_caching_min_ttl = 5
+  }
+
+  custom_error_response {
+    error_code            = 404
+    error_caching_min_ttl = 5
+  }
+
+  custom_error_response {
+    error_code            = 500
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 501
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 502
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 503
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 504
+    error_caching_min_ttl = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = data.terraform_remote_state.upselling_infrastructure.outputs.certificate_arn
+    minimum_protocol_version = "TLSv1.2_2018"
+    ssl_support_method       = "sni-only"
+  }
+}
+
 resource "aws_route53_health_check" "storefront_api" {
   fqdn              = aws_ssm_parameter.storefront_api_regional_domain.value
   port              = 443
@@ -51,7 +141,7 @@ resource "aws_route53_record" "storefront_api" {
   type            = "CNAME"
   ttl             = "86400"
   set_identifier  = data.aws_region.current.name
-  records         = [aws_ssm_parameter.storefront_api_regional_domain.value]
+  records         = [aws_cloudfront_distribution.storefront_api.domain_name]
   health_check_id = aws_route53_health_check.storefront_api.id
 
   latency_routing_policy {
