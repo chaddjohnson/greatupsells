3e7f13fd3cdd2110aec7d098c86e9602c22f32f8
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-29 06:17:37 -0400

    Added migration to create collections.

---
+++ services/shops/migrations/20210929100149-create-collections.js
@@ -0,0 +1,31 @@
+const models = require('../src/models');
+
+module.exports = {
+  async up() {
+    const Collection = await models.get('Collection');
+    const OfferHit = await models.get('OfferHit');
+    const Offer = await models.get('Offer');
+    const Order = await models.get('Order');
+    const PopupTheme = await models.get('PopupTheme');
+    const Product = await models.get('Product');
+    const Shop = await models.get('Shop');
+    const Stats = await models.get('Stats');
+    const User = await models.get('User');
+
+    await Collection.createCollection();
+    await OfferHit.createCollection();
+    await Offer.createCollection();
+    await Order.createCollection();
+    await PopupTheme.createCollection();
+    await Product.createCollection();
+    await Shop.createCollection();
+    await Stats.createCollection();
+    await User.createCollection();
+  },
+
+  async down() {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  }
+};
