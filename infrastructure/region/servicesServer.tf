7d78d47680a191e435492c2b772f6b1c3f5f941a
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-29 14:22:02 -0400

    Reduced services DNS TTL to 1 hour.

--- infrastructure/region/servicesServer.tf
+++ infrastructure/region/servicesServer.tf
@@ -127,6 +127,6 @@ resource "aws_route53_record" "domain_name" {
   zone_id = var.hosted_zone_id
   name    = var.services_domain_name
   type    = "A"
-  ttl     = 86400
+  ttl     = 3600
   records = [aws_eip.services_server.public_ip]
 }
