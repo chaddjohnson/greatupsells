cd2f00c0eb3804ea01e5be188dff84517da0d4a9
Chad Johnson <chad.d.johnson@gmail.com>
2021-09-29 20:56:43 -0400

    Updated Shopify Admin health check to include a Redis connectivity check.

--- applications/shopify-admin/src/routes/health.js
+++ applications/shopify-admin/src/routes/health.js
@@ -1,6 +1,9 @@
 const middy = require('@middy/core');
 const cors = require('@middy/http-cors');
+const redis = require('redis');
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+
+const { REDIS_URL_APP } = process.env;
 
 const handler = middy(async (event) => {
   if (event.source === 'serverless-plugin-warmup') {
@@ -8,10 +11,21 @@ const handler = middy(async (event) => {
     return 'Lambda is warm!';
   }
 
-  return {
-    statusCode: StatusCodes.OK,
-    body: ReasonPhrases.OK
-  };
+  try {
+    redis.createClient({ url: REDIS_URL_APP });
+
+    return {
+      statusCode: StatusCodes.OK,
+      body: ReasonPhrases.OK
+    };
+  } catch (error) {
+    console.error(error.stack); // eslint-disable-line no-console
+
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+    };
+  }
 });
 
 handler.use(cors());
