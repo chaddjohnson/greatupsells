2981d305cf65ea38c60c1a5512e28d8d08df61ac
Chad Johnson <chad.d.johnson@gmail.com>
2021-10-10 19:36:01 -0400

    Increased delay and attempts for conversion tracking.

--- services/shops/src/models/Order/trackConversions.js
+++ services/shops/src/models/Order/trackConversions.js
@@ -21,12 +21,16 @@ const trackConversions = async (order) => {
   const { shopifyOrderId } = order;
   let offerHits = [];
   let attempts = 0;
-  const conditionFn = () => offerHits.length === 0 && attempts < 3;
+  const conditionFn = () => offerHits.length === 0 && attempts < 7;
 
+  // Try multiple times to find offer hits for the shopify order. This is due
+  // to a race condition where that an order may be created at the same time as
+  // a draft order being updated. In draft order updates, offer hits are
+  // associated with completed orders.
   await promiseWhile(conditionFn, async () => {
     offerHits = await OfferHit.find({ shopifyOrderId });
     attempts++;
-    await Promise.delay(500);
+    await Promise.delay(1 * 1000);
   });
 
   if (offerHits.length === 0) {
