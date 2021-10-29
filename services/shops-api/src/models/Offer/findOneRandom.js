22e2129031a9adf97ff4764cb4da200b85c3e8a1
Chad Johnson <chad.d.johnson@gmail.com>
2021-10-28 23:29:53 -0400

    Removed debugging.

--- services/shops-api/src/models/Offer/findOneRandom.js
+++ services/shops-api/src/models/Offer/findOneRandom.js
@@ -233,8 +233,6 @@ const findOneRandom = async (
   });
   const pagePathSanitized = sanitizePagePath(pagePath);
 
-  console.log(JSON.stringify(criteria));
-
   // Randomly find an offer.
   let offers = await Offer.find(criteria);
 
