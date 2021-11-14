e2e01fd61db0b449a07d33d21bf0ae4f80bccd6a
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-14 15:19:22 -0500

    Opened up port 9300 for Elasticsearch.

--- infrastructure/region/servicesServer.tf
+++ infrastructure/region/servicesServer.tf
@@ -59,6 +59,14 @@ resource "aws_security_group" "services_server" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Elasticsearch clustering
+  ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
