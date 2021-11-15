a7051c7b89529c52f09835863b35c22e59291106
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-14 21:59:48 -0500

    Saving to Elasticsearch via Log model.

--- services/logs/src/models/Log.js
+++ services/logs/src/models/Log.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
+const { Client: ElasticsearchClient } = require('@elastic/elasticsearch');
 const mongodbClient = require('./mongodbClient');
+
+const { ELASTICSEARCH_URL } = process.env;
+
+const esClient = new ElasticsearchClient({
+  node: ELASTICSEARCH_URL
+});
 
 let Log = null;
 
@@ -14,6 +21,30 @@ const schema = new mongoose.Schema({
   stackTrace: { type: String, required: false, trim: true },
   data: { type: mongoose.Schema.Types.Mixed, required: false },
   createdAt: { type: Date, required: true, default: Date.now }
+});
+
+schema.post('save', async function (log, next) {
+  const { id, source, type, message } = log;
+  const createdAt = log.createdAt.toISOString();
+
+  // Index log data (minus data) in Elasticsearch. Note that Elasticsearch is
+  // ONLY used as an index into the MongoDB "logs" collection and `data` is NOT
+  // stored in Elasticsearch.
+  await esClient.index({
+    index: 'logs',
+    id,
+    body: {
+      source,
+      type,
+      message,
+      createdAt
+    }
+  });
+
+  // Force an index refresh to ensure the logs are included in subsequent searches.
+  await esClient.indices.refresh({ index: 'logs' });
+
+  next();
 });
 
 Log = mongodbClient.connection.model('Log', schema);
--- services/logs/src/workers/log.js
+++ services/logs/src/workers/log.js
@@ -1,11 +1,4 @@
-const { Client: ElasticsearchClient } = require('@elastic/elasticsearch');
 const models = require('../models');
-
-const { ELASTICSEARCH_URL } = process.env;
-
-const esClient = new ElasticsearchClient({
-  node: ELASTICSEARCH_URL
-});
 
 const processRecord = async (record) => {
   const Log = await models.get('Log');
@@ -14,26 +7,7 @@ const processRecord = async (record) => {
   const { source, type, message, stackTrace, data } = JSON.parse(record.body);
 
   // Create log in MongoDB. Middleware will then create Elasticsearch document.
-  const log = await Log.create({ source, type, message, stackTrace, data });
-  const { id } = log;
-  const createdAt = log.createdAt.toISOString();
-
-  // Index log data (minus data) in Elasticsearch. Note that Elasticsearch is
-  // ONLY used as an index into the MongoDB "logs" collection and `data` is NOT
-  // stored in Elasticsearch.
-  await esClient.index({
-    index: 'logs',
-    id,
-    body: {
-      source,
-      type,
-      message,
-      createdAt
-    }
-  });
-
-  // Force an index refresh to ensure the logs are included in subsequent searches.
-  await esClient.indices.refresh({ index: 'logs' });
+  await Log.create({ source, type, message, stackTrace, data });
 
   // Enqueue an email notification for error logs.
   if (type === 'ERROR') {
