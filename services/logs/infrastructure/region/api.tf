972d509124b4bd52f20524197ba8635ad2b022ba
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-21 13:13:28 -0500

    Improved logs service infrastructure.

--- services/logs/infrastructure/region/api.tf
+++ services/logs/infrastructure/region/api.tf
@@ -1,30 +1,5 @@
 locals {
-  mongodb_hosts = "${join(":27017,", data.terraform_remote_state.greatupsells_infrastructure.outputs.services_domain_names)}:27017"
-  domain        = "logs-api.${data.aws_region.current.name}.${data.terraform_remote_state.greatupsells_infrastructure.outputs.domain}"
-}
-
-resource "aws_ssm_parameter" "elasticsearch_logs_database_url_production" {
-  count     = terraform.workspace == "production" ? 1 : 0
-  name      = "/greatupsells/${terraform.workspace}/database/elasticsearch-logs/url"
-  type      = "SecureString"
-  overwrite = true
-  value     = "https://app:${var.elasticsearch_app_password}@services.${data.aws_region.current.name}.${data.terraform_remote_state.greatupsells_infrastructure.outputs.domain}:9200"
-}
-
-resource "aws_ssm_parameter" "elasticsearch_logs_database_url_test" {
-  count     = terraform.workspace == "test" ? 1 : 0
-  name      = "/greatupsells/${terraform.workspace}/database/elasticsearch-logs/url"
-  type      = "SecureString"
-  overwrite = true
-  value     = "https://app:${var.elasticsearch_app_password}@services.${data.terraform_remote_state.greatupsells_infrastructure.outputs.domain}:9200"
-}
-
-resource "aws_ssm_parameter" "mongodb_logs_database_url" {
-  name      = "/greatupsells/${terraform.workspace}/database/mongodb-logs/url"
-  type      = "SecureString"
-  value     = "mongodb://app:${var.mongodb_app_password}@${local.mongodb_hosts}/greatupsells-logs?replicaSet=rs0&readPreference=secondaryPreferred&w=1&wtimeoutMS=5000&ssl=true"
-  overwrite = true
-  provider  = aws.region
+  domain = "logs-api.${data.aws_region.current.name}.${data.terraform_remote_state.greatupsells_infrastructure.outputs.domain}"
 }
 
 resource "aws_ssm_parameter" "logs_api_regional_domain" {
