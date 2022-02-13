f9439ac5e801176597d965073c61cbab19ba87da
Chad Johnson <chad.d.johnson@gmail.com>
2022-02-13 05:37:38 -0500

    removed debugging

--- services/logs/src/workers/log.js
+++ services/logs/src/workers/log.js
@@ -6,8 +6,6 @@ const { DOMAIN, LOGS_NOTIFICATION_EMAIL } = process.env;
 const processRecord = async (record) => {
   const Log = await models.get('Log');
 
-  console.log(record.body);
-
   // Parse the message.
   const { source, type, message, stackTrace, data } = JSON.parse(record.body);
 
