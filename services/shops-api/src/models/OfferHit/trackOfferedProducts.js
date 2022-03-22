153620cf2933af502abce2892a0291824ecf75f1
Chad Johnson <chad.d.johnson@gmail.com>
2022-03-22 15:07:42 -0400

    Fixed issue causing offerHit.offeredProducts to not be recorded.

--- services/shops-api/src/models/OfferHit/trackOfferedProducts.js
+++ services/shops-api/src/models/OfferHit/trackOfferedProducts.js
@@ -2,13 +2,18 @@ const models = require('..');
 
 const trackOfferedProducts = async (offerHit, shopifyProductIds = []) => {
   const OfferHit = await models.get('OfferHit');
+  const session = await offerHit.$session();
 
   // Track the viewed product data for the offer hit.
   const offeredProducts = shopifyProductIds.map((shopifyProductId) => ({
     shopifyProductId
   }));
 
-  await OfferHit.findByIdAndUpdate(offerHit.id, { offeredProducts });
+  await OfferHit.findByIdAndUpdate(
+    offerHit.id,
+    { offeredProducts },
+    { session }
+  );
 };
 
 module.exports = trackOfferedProducts;
