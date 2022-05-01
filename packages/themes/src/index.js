084e2a399d058cd5d5fc2aa826a5588bf0ebb963
Chad Johnson <chad.d.johnson@gmail.com>
2022-05-01 12:21:37 -0400

    Reorganized themes package files.

--- packages/themes/src/index.js
+++ packages/themes/src/index.js
@@ -1,3 +1,4 @@
 // TODO Do not always include all themes. Build separate packages.
 
 export { default as MultiProductOffer1 } from './MultiProductOffer1';
+export { default as SingleProductOffer1 } from './SingleProductOffer1';
