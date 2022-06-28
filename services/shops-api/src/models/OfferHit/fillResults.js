5c484d59c3671b4deaba54e96b7571bf7ca34d98
Chad Johnson <chad.d.johnson@gmail.com>
2022-06-27 22:12:41 -0400

    Improved zero-filling of chart date.

--- services/shops-api/src/models/OfferHit/fillResults.js
+++ services/shops-api/src/models/OfferHit/fillResults.js
@@ -7,27 +7,29 @@ const fillResults = (results, valueKey) => {
   }
 
   const firstResult = min(results, (result) => new Date(result.date));
-  const firstDate = DateTime.fromISO(firstResult.date);
   const today = DateTime.fromISO('12:00:00Z');
+  const firstDate = firstResult ? DateTime.fromISO(firstResult.date) : today;
   const days = Interval.fromDateTimes(firstDate, today).length('days');
 
-  const newResults = [...Array(days).keys()].map((index) => {
-    const targetDateString = firstDate.plus({ days: index }).toISODate();
-    let resultDate = results.find((result) => {
-      const resultDateString = DateTime.fromISO(result.date).toISODate();
+  const newResults = [...Array(days).keys()]
+    .map((index) => {
+      const targetDateString = firstDate.plus({ days: index }).toISODate();
+      let resultDate = results.find((result) => {
+        const resultDateString = DateTime.fromISO(result.date).toISODate();
 
-      return resultDateString === targetDateString;
-    });
+        return resultDateString === targetDateString;
+      });
 
-    resultDate = resultDate || {
-      date: DateTime.fromISO(`${targetDateString}T12:00:00Z`)
-        .toJSDate()
-        .toISOString(),
-      [valueKey]: 0
-    };
+      resultDate = resultDate || {
+        date: DateTime.fromISO(`${targetDateString}T12:00:00Z`)
+          .toJSDate()
+          .toISOString(),
+        [valueKey]: 0
+      };
 
-    return resultDate;
-  });
+      return resultDate;
+    })
+    .concat(results);
 
   return newResults;
 };
