694b8d76afd5a14e5e7282365461c8dbd656db56
Chad Johnson <chad.d.johnson@gmail.com>
2022-12-05 02:00:30 +0100

    debugging

--- services/logs/migrate-mongo-config.js
+++ services/logs/migrate-mongo-config.js
@@ -5,6 +5,8 @@ dotenvExpand(dotenv.config());
 
 const { MONGODB_URL_LOGS } = process.env;
 
+console.log(`MONGODB_URL_LOGS = ${MONGODB_URL_LOGS}`);
+
 const config = {
   mongodb: {
     url: MONGODB_URL_LOGS,
