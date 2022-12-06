8471fe1b9eebf214d6ab44c3b8450fda37fa67d0
Chad Johnson <chad.d.johnson@gmail.com>
2022-12-06 14:20:17 +0100

    Removed debugging

--- applications/shopify-admin/src/components/TitleBar.js
+++ applications/shopify-admin/src/components/TitleBar.js
@@ -12,10 +12,7 @@ const TitleBar = (props) => {
   const secondaryActions = [
     {
       content: 'Dashboard',
-      url: `/${urlParams}`,
-      onAction: () => {
-        localStorage.setItem('action', 'value');
-      }
+      url: `/${urlParams}`
     },
     {
       content: 'Offers',
