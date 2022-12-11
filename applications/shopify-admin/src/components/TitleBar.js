a4bc2bf135f3a4e3f8e421592e7ee33492668ce7
Chad Johnson <chad.d.johnson@gmail.com>
2022-12-11 01:01:03 +0100

    Revert "Fixed button order in titlebar."
    This reverts commit aa04cb61a1db547988ec6459be3fb694c8c5bbfe.

--- applications/shopify-admin/src/components/TitleBar.js
+++ applications/shopify-admin/src/components/TitleBar.js
@@ -11,12 +11,12 @@ const TitleBar = (props) => {
   };
   const secondaryActions = [
     {
-      content: 'Offers',
-      url: `/offers/${urlParams}`
-    },
-    {
       content: 'Dashboard',
       url: `/${urlParams}`
+    },
+    {
+      content: 'Offers',
+      url: `/offers/${urlParams}`
     }
   ];
   // const actionGroups = [
