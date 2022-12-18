8d86bd4ac600a8ccec90d65079b9e562de3b6a0a
Chad Johnson <chad.d.johnson@gmail.com>
2022-12-18 22:29:17 +0100

    Updated health check request interval.

--- applications/shopify-admin/infrastructure/region/main.tf
+++ applications/shopify-admin/infrastructure/region/main.tf
@@ -50,7 +50,7 @@ resource "aws_route53_health_check" "shopify_admin_app" {
   type              = "HTTPS"
   resource_path     = "/health"
   failure_threshold = "5"
-  request_interval  = "60"
+  request_interval  = "30"
   regions           = ["us-east-1", "eu-west-1", "ap-northeast-1"]
 
   tags = {
