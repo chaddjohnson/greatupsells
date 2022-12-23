265be77019a36505575a0b0f2334c509835f78b7
Chad Johnson <chad.d.johnson@gmail.com>
2022-12-23 21:37:40 +0100

    Fixed sourceIp reference under HTTP API.

--- services/storefront-api/src/routes/trackOfferImpression.js
+++ services/storefront-api/src/routes/trackOfferImpression.js
@@ -21,8 +21,7 @@ const handler = middy(async (event, context) => {
 
   try {
     const ipAddress =
-      event.requestContext.identity.sourceIp ||
-      event.headers['X-Forwarded-For'];
+      event.requestContext.http.sourceIp || event.headers['X-Forwarded-For'];
     const domain = new URL(
       event.headers.shop || event.headers.origin || event.headers.Origin
     ).host;
