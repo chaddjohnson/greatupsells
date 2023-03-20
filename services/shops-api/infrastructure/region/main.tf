7ca4bd179ead86f0890336bef5eef16a9722b9a8
Chad Johnson <chad.d.johnson@gmail.com>
2023-03-20 14:05:56 +0100

    Changed how alarm is associated with health check

--- services/shops-api/infrastructure/region/main.tf
+++ services/shops-api/infrastructure/region/main.tf
@@ -20,6 +20,21 @@ data "terraform_remote_state" "greatupsells_infrastructure" {
   }
 }
 
+resource "aws_route53_health_check" "shops_api" {
+  fqdn                            = local.domain
+  port                            = 443
+  type                            = "HTTPS"
+  resource_path                   = "/health"
+  failure_threshold               = "5"
+  request_interval                = "30"
+  regions                         = ["us-east-1", "eu-west-1", "ap-northeast-1"]
+  insufficient_data_health_status = "Healthy"
+
+  tags = {
+    Name = "shops-api-${terraform.workspace}"
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "shops_api" {
   alarm_name          = "shops-api-alarm-${terraform.workspace}"
   comparison_operator = "LessThanThreshold"
@@ -29,25 +44,11 @@ resource "aws_cloudwatch_metric_alarm" "shops_api" {
   period              = "60"
   statistic           = "Minimum"
   threshold           = "18"
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.shops_api.id
+  }
   # alarm_actions       = [aws_sns_topic.sns.arn]
   # ok_actions          = [aws_sns_topic.sns.arn]
-}
-
-resource "aws_route53_health_check" "shops_api" {
-  fqdn                            = local.domain
-  port                            = 443
-  type                            = "HTTPS"
-  resource_path                   = "/health"
-  failure_threshold               = "5"
-  request_interval                = "30"
-  regions                         = ["us-east-1", "eu-west-1", "ap-northeast-1"]
-  cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.shops_api.alarm_name
-  cloudwatch_alarm_region         = data.aws_region.current.name
-  insufficient_data_health_status = "Healthy"
-
-  tags = {
-    Name = "shops-api-${terraform.workspace}"
-  }
 }
 
 resource "aws_ssm_parameter" "shops_api_health_check_id" {
