a8c9c107fbc253ea1eaf962067ac7e7675030828
Chad Johnson <chad.d.johnson@gmail.com>
2023-03-20 09:37:23 +0100

    Implemented health check for Shops API

--- services/shops-api/infrastructure/region/main.tf
+++ services/shops-api/infrastructure/region/main.tf
@@ -19,3 +19,25 @@ data "terraform_remote_state" "greatupsells_infrastructure" {
     region = "us-east-1"
   }
 }
+
+resource "aws_route53_health_check" "shops_api" {
+  fqdn              = local.domain
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/health"
+  failure_threshold = "5"
+  request_interval  = "30"
+  regions           = ["us-east-1", "eu-west-1", "ap-northeast-1"]
+
+  tags = {
+    Name = "shops-api-${terraform.workspace}"
+  }
+}
+
+resource "aws_ssm_parameter" "shops_api_health_check_id" {
+  name      = "/greatupsells/${terraform.workspace}/shops-api/health-check-id"
+  type      = "String"
+  value     = aws_route53_health_check.shops_api.id
+  overwrite = true
+  provider  = aws.region
+}
