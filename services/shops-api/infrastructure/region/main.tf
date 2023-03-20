8b661fe1a7807da7779b125aec8993ee443db9f1
Chad Johnson <chad.d.johnson@gmail.com>
2023-03-20 13:40:58 +0100

    Associated alarm with Shops API health check

--- services/shops-api/infrastructure/region/main.tf
+++ services/shops-api/infrastructure/region/main.tf
@@ -20,14 +20,29 @@ data "terraform_remote_state" "greatupsells_infrastructure" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "shops_api" {
+  alarm_name          = "shops-api-alarm-${terraform.workspace}"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "3"
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/Route53"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "18"
+  # alarm_actions       = [aws_sns_topic.sns.arn]
+  # ok_actions          = [aws_sns_topic.sns.arn]
+}
+
 resource "aws_route53_health_check" "shops_api" {
-  fqdn              = local.domain
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/health"
-  failure_threshold = "5"
-  request_interval  = "30"
-  regions           = ["us-east-1", "eu-west-1", "ap-northeast-1"]
+  fqdn                            = local.domain
+  port                            = 443
+  type                            = "HTTPS"
+  resource_path                   = "/health"
+  failure_threshold               = "5"
+  request_interval                = "30"
+  regions                         = ["us-east-1", "eu-west-1", "ap-northeast-1"]
+  cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.shops_api.alarm_name
+  insufficient_data_health_status = "Healthy"
 
   tags = {
     Name = "shops-api-${terraform.workspace}"
@@ -40,17 +55,4 @@ resource "aws_ssm_parameter" "shops_api_health_check_id" {
   value     = aws_route53_health_check.shops_api.id
   overwrite = true
   provider  = aws.region
-}
-
-resource "aws_cloudwatch_metric_alarm" "shops_api_health_check_alarm" {
-  alarm_name          = "shops-api-alarm-${terraform.workspace}"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "3"
-  metric_name         = "HealthyHostCount"
-  namespace           = "AWS/Route53"
-  period              = "60"
-  statistic           = "Minimum"
-  threshold           = "18"
-  # alarm_actions       = [aws_sns_topic.sns.arn]
-  # ok_actions          = [aws_sns_topic.sns.arn]
 }
