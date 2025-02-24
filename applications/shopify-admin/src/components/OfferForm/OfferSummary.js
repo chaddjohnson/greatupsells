2581043d1664becb0e218ee20936ec0c3706b2f7
Chad Johnson <chad.d.johnson@gmail.com>
2025-02-24 17:39:54 +0100

    Improved offer summary

--- applications/shopify-admin/src/components/OfferForm/OfferSummary.js
+++ applications/shopify-admin/src/components/OfferForm/OfferSummary.js
@@ -3,8 +3,31 @@ import PropTypes from 'prop-types';
 import { Card, Text, List, BlockStack, InlineStack } from '@shopify/polaris';
 import { useNumberFormatter, useCurrency, useDateTime } from '@greatupsells/react-hooks';
 import { useShop } from '../../hooks';
-import Link from '../Link';
 import OfferStatus from '../OfferStatus';
+
+const strategyMap = {
+  CROSS_SELL: 'Cross-sell',
+  UPSELL: 'Upsell',
+  POST_PURCHASE: 'Post-purchase',
+  THANK_YOU_PAGE: 'Thank You page',
+  ORDER_STATUS_PAGE: 'Order Status page',
+  POPUP: 'Popup'
+};
+
+const triggerEventMap = {
+  ADD: 'Add to cart',
+  EXIT: 'Exit intent',
+  LOAD: 'Page load',
+  FOCUS: 'Lost browser focus',
+  SCROLL: 'Page scroll',
+  LINK: 'Link click'
+};
+
+const triggerPageMap = {
+  ANY: 'Any page',
+  PAGE: 'Specific page',
+  PATTERN: 'URL pattern'
+};
 
 const OfferSummary = ({ offer }) => {
   const { shop } = useShop();
@@ -20,6 +43,11 @@ const OfferSummary = ({ offer }) => {
   const items = useMemo(() => {
     const newItems = [];
 
+    newItems.push(`${strategyMap[offer.strategy]} strategy`);
+    newItems.push(`${triggerEventMap[offer.triggerEvent]} trigger`);
+    newItems.push(`${triggerPageMap[offer.triggerPage]} as trigger`);
+
+    // Discount type
     if (offer.discountType === 'PERCENTAGE' && offer.discountValue) {
       newItems.push(`${offer.discountValue * 100}% off accepted products`);
     }
@@ -30,6 +58,7 @@ const OfferSummary = ({ offer }) => {
       newItems.push(`${formatCurrency(offer.discountValue)} for each accepted product`);
     }
 
+    // Minimum requirement
     if (offer.minimumRequirement === 'AMOUNT' && offer.minimumRequiredAmount) {
       newItems.push(`Minimum purchase of ${formatCurrency(offer.minimumRequiredAmount)}`);
     }
@@ -37,10 +66,19 @@ const OfferSummary = ({ offer }) => {
       newItems.push(`Minimum purchase of ${offer.minimumRequiredAmount} items`);
     }
 
+    // Dates
     if (offer.startAt && offer.endAt) {
       newItems.push(`Active from ${formatDate(offer.startAt, 'MMM d, y')} to ${formatDate(offer.endAt, 'MMM d, y')}`);
     } else if (offer.startAt) {
       newItems.push(`Active from ${formatDate(offer.startAt, 'MMM d, y')}`);
+    }
+
+    if (offer.enableBundling) {
+      newItems.push(`Products offered in bundle`);
+    }
+
+    if (offer.geotargetingCountries?.length > 0) {
+      newItems.push(`Geotarget countries: ${offer.geotargetingCountries.join(', ')}`);
     }
 
     return newItems;
@@ -53,9 +91,7 @@ const OfferSummary = ({ offer }) => {
         {offer.name ? (
           <BlockStack gap="200">
             <InlineStack align="space-between">
-              <Text variant="headingMd" as="h2">
-                {offer.name}
-              </Text>
+              <Text variant="headingMd">{offer.name}</Text>
               <OfferStatus offer={offer} />
             </InlineStack>
             {items.length > 0 && (
@@ -79,14 +115,6 @@ const OfferSummary = ({ offer }) => {
               <List.Item>{formatCurrency(offer?.revenueIncrease)} revenue increase</List.Item>
               {/* <List.Item>44 data submissions</List.Item> */}
             </List>
-            <Text tone="subdued">
-              View <Link url={`/offers/${offer._id}/analytics/`}>analytics</Link>{' '}
-              {/* and{' '}
-                <Button variant="plain" url={`/offers/${offer._id}/data/`}>
-                  data submissions
-                </Button>{' '} */}
-              for this offer
-            </Text>
           </>
         )}
       </BlockStack>
