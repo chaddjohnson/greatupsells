4f3da6ec30f47585e6d068f7cc92efc43a0917ea
Chad Johnson <chad.d.johnson@gmail.com>
2025-02-27 23:10:40 +0100

    Updated Terraform config for logs service

--- services/logs/infrastructure/main.tf
+++ services/logs/infrastructure/main.tf
@@ -27,6 +27,9 @@ provider "aws" {
 
 module "us_east_1" {
   source = "./region"
+  providers = {
+    aws.region = aws
+  }
 
   elasticsearch_app_password = var.elasticsearch_app_password
   mongodb_app_password       = var.mongodb_app_password
