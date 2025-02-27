0f131e7ac8dc5819006a3b9c5bd226bc49f45446
Chad Johnson <chad.d.johnson@gmail.com>
2025-02-27 23:06:06 +0100

    Updated Terraform config for logs service

--- services/logs/infrastructure/main.tf
+++ services/logs/infrastructure/main.tf
@@ -6,10 +6,15 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "us-east-1"
 }
 
+provider "aws" {
+  alias  = "region"
+  region = "us-east-1"
+}
 # provider "aws" {
 #   alias  = "eu-west-1"
 #   region = "eu-west-1"
