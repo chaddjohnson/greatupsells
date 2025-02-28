16d043e0fed89a58fbfa3d7ae741eff4649f227d
Chad Johnson <chad.d.johnson@gmail.com>
2025-02-28 22:37:49 +0100

    debugging

--- services/logs/migrate-mongo-config.js
+++ services/logs/migrate-mongo-config.js
@@ -1,4 +1,5 @@
-console.log(JSON.stringify(process.env));
+console.log(`MONGODB_URL_LOGS = ${process.env.MONGODB_URL_LOGS}`);
+console.log(`ELASTICSEARCH_URL = ${process.env.ELASTICSEARCH_URL}`);
 
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
