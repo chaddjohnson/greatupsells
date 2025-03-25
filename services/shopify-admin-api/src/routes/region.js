b2060765470683c98abceb71324fb9df06a99d96
Chad Johnson <chad.d.johnson@gmail.com>
2025-03-26 00:09:12 +0100

    Removed test code

--- services/shopify-admin-api/src/routes/region.js
+++ services/shopify-admin-api/src/routes/region.js
@@ -1,33 +1,13 @@
 const middy = require('@middy/core');
 const cors = require('@middy/http-cors');
-const { StatusCodes, ReasonPhrases } = require('http-status-codes');
-const logger = require('@greatupsells/logger');
+const { StatusCodes } = require('http-status-codes');
 
 const { AWS_REGION } = process.env;
 
-// const handler = middy(async () => ({
-//   statusCode: StatusCodes.OK,
-//   body: AWS_REGION
-// }));
-
-// handler.use(cors());
-
-// module.exports.handler = handler;
-
-const handler = middy(async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  try {
-    throw new Error('Test error');
-  } catch (error) {
-    await logger.error(error.message, error, { event });
-  }
-
-  return {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    body: ReasonPhrases.INTERNAL_SERVER_ERROR
-  };
-});
+const handler = middy(async () => ({
+  statusCode: StatusCodes.OK,
+  body: AWS_REGION
+}));
 
 handler.use(cors());
 
