116d23176fb370ddb372b163cedf85fdaddfce0c
Chad Johnson <chad.d.johnson@gmail.com>
2025-04-18 22:05:16 +0200

    Ignoring certificate changes for CloudFront distributions

--- applications/admin/infrastructure/cdn.tf
+++ applications/admin/infrastructure/cdn.tf
@@ -138,6 +138,10 @@ resource "aws_cloudfront_distribution" "admin" {
     minimum_protocol_version = "TLSv1.2_2018"
     ssl_support_method       = "sni-only"
   }
+
+  lifecycle {
+    ignore_changes = [viewer_certificate]
+  }
 }
 
 resource "aws_route53_record" "admin" {
--- infrastructure/assets.tf
+++ infrastructure/assets.tf
@@ -128,6 +128,10 @@ resource "aws_cloudfront_distribution" "assets" {
     minimum_protocol_version = "TLSv1.2_2018"
     ssl_support_method       = "sni-only"
   }
+
+  lifecycle {
+    ignore_changes = [viewer_certificate]
+  }
 }
 
 resource "aws_route53_record" "assets" {
